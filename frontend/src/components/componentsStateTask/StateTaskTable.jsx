630dc5ea05206b736b5b77cbd1ca331f5cab4b93
sr_zurab <sr-zurab@ya.ru>
2025-08-29 15:52:15 +0300

    обновление 29.08.2025

--- frontend/src/components/componentsStateTask/StateTaskTable.jsx
+++ frontend/src/components/componentsStateTask/StateTaskTable.jsx
@@ -3,8 +3,41 @@ import React from "react";
 
 const TableStateTask = ({headers, subheaders1, subheaders2, cols, data, onDataChange, onCellUpdate, onDeleteRow}) =>{
 
+    //Обновление строки и ячеек
+    const handleChange = (rowIndex, field, value) => {
+        const updated = [...data];
+        const currentRow = updated[rowIndex];
 
+        updated[rowIndex] = {
+            ...currentRow,
+            [field]: value,
+        };
 
+        onDataChange(updated);
+
+        if (currentRow.id && onCellUpdate) {
+            const parsedValue = value === '' ? null : value;
+            onCellUpdate(currentRow.id, field, parsedValue);
+        }
+    };
+
+    //Управление таблицей с помощью кнопок
+    const handleKeyDown = (e, rowIndex, colIndex) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            const next = document.querySelector(`[data-cell="${rowIndex + 1}-${colIndex}"]`);
+            if (next) next.focus();
+        }
+    };
+
+    //Вызов обновления ячейки после ввода пользователя
+    const handleBlur = (rowIndex, value, data, onCellUpdate, field) => {
+        const currentRow = data[rowIndex];
+        const parsedValue = value === "" ? null : value;
+        if (currentRow.id && typeof onCellUpdate === "function") {
+            onCellUpdate(currentRow.id, field, parsedValue);
+        }
+    };
 
 
     return(
@@ -32,16 +65,16 @@ const TableStateTask = ({headers, subheaders1, subheaders2, cols, data, onDataCh
                              )}
                     </thead>
                     <tbody>
-                        {data.map((row,rowindex)=>
-                            <tr key={rowindex}>
-                                {cols.map((col,colindex) =>(
-                                    <td key= {`${rowindex}-${colindex}`}>
+                        {data.map((row,rowIndex)=>
+                            <tr key={rowIndex}>
+                                {cols.map((col,colIndex) =>(
+                                    <td key= {`${rowIndex}-${colIndex}`}>
                                         <textarea
                                             value={row[col.field] || ""}
-                                            onChange={() => {}}
-                                            onBlur={() => {}}
-                                            onKeyDown={() => {}}
-                                            data-cell={`${col.field}-${colindex}`}
+                                            onChange={e => handleChange(rowIndex, col.field, e.target.value)}
+                                            onBlur={e => {handleBlur(rowIndex, col.field, e.target.value)}}
+                                            onKeyDown={e => {handleKeyDown(e,rowIndex,colIndex)}}
+                                            data-cell={`${col.field}-${colIndex}`}
                                         />
                                     </td>
                                 ))}
